9c9b6a39aa79601dae5a36fe947b23cfc333459c
Deepesh Nair <hybridx@gmail.com>
2018-08-31 17:12:40 +0530

    POC done

--- intitle_crawler.py
+++ intitle_crawler.py
@@ -5,7 +5,7 @@ import pymysql as MySQLdb
 
 #url = 'https://theswissbay.ch/pdf/Gentoomen%20Library/Programming/Python/'
 
-db = MySQLdb.connect("localhost","root","root","books")
+db = MySQLdb.connect("localhost","admin","root","books")
 cursor = db.cursor()
 
 my_url = []
@@ -18,12 +18,14 @@ while True:
 	else:
 		my_url.append(urls)
 
+print("Getting Data...and Crawling the page magically.....")
 
 for url in my_url:
 	try:
 		uClient = uReq(url)
 		page_html = uClient.read()
 		uClient.close()
+		print("Parsing URL...")
 		page_soup = soup(page_html, "html.parser")
 		links = page_soup.findAll("a",href=True)
 	except:
@@ -31,6 +33,7 @@ for url in my_url:
 
 	for link in links:
 		completeLink = url+link["href"]
+		print(link)
 		sql = "insert into book_links(book_name,book_link) values(\"%s\",\"%s\")" % (str(link.text),str(completeLink)) 
 		try:
 			cursor.execute(sql)
@@ -38,3 +41,4 @@ for url in my_url:
 		except:
 			print("This ended in an error ----> ",link.text , " ------>",completeLink)
 db.close()
+print("Thank you....")
